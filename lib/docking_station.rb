ec00890fc5349a136a682b7cb7c8a88fa7edd0ad
Timothy Robinson <timmyrobby@gmail.com>
2017-04-11 16:42:08 +0100

    Completes step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,19 @@
-require 'bike.rb'
+require_relative "bike"
 
 class DockingStation
+  attr_reader :bike
   def initialize
   end
+
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  def bike
+    @bike
   end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -4,8 +4,17 @@ describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
 
   it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    expect(subject.release_bike).to be_working
+  end
+
+  # Checking that you can dock bikes
+  it {is_expected.to respond_to(:dock)}
+
+  # Checking that it will return docked bikes when viewed.
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
 
