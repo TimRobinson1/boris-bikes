3cce2781946095bc91a1146414f09eb2cae12883
Timothy Robinson <timmyrobby@gmail.com>
2017-04-12 11:38:58 +0100

    Adds step 13 & 14

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -16,11 +16,16 @@ describe DockingStation do
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.release_bike).to eq bike
   end
 
   it 'raises an error if there are no bikes' do
     expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+
+  it 'raises an error if station is full when docking' do
+    20.times { subject.dock(Bike.new) }
+    expect { subject.dock(Bike.new) }.to raise_error 'Station full'
   end
 end
 
